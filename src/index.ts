2935d6dddcdd28514190c5144a14024f8dd980de
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-11-25 22:14:18 -0600

    [sm] resolved some issues with postgres and troubleshooting entities and connection still

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
-import { MikroORM } from "@mikro-orm/core"
+import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+
+console.log("PROCESS.ENV")
+console.log(process.env)
+
+// import password from .env at root
+const PASSWORD = process.env.PASSWORD;
+const USERNAME = process.env.USERNAME;
 
 const main = async () => {
   const orm = await MikroORM.init({
     entities: [Post],
-    dbName: "reddit-server",
+    dbName: "redditserver",
     type: "postgresql",
+    user: USERNAME,
+    password: PASSWORD,
     debug: !__prod__,
   });
+
+  console.log("---SQL ONE---");
+  const post = orm.em.create(Post, {title: 'my first post'});
+  await orm.em.persistAndFlush(post);
+  console.log("---SQL TWO---");
+  await orm.em.nativeInsert(Post, {title: 'my second post'});
 
 };
 
